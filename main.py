7fa876e14ee6c82a675cf3866be74769dacfe394
Lukas P <papikdev@gmail.com>
2019-06-08 12:05:32 +0200

    video encryption support

--- main.py
+++ main.py
@@ -4,7 +4,8 @@ import random
 import sys
 from argparse import ArgumentParser
 import os
-
+from pydub import AudioSegment
+from concurrent.futures import ThreadPoolExecutor
 if os.name == 'nt':
     import ctypes
 
@@ -14,6 +15,8 @@ parser = ArgumentParser(description='Encrypt image with code')
 group = parser.add_mutually_exclusive_group()
 group.add_argument('-e', '--encrypt')
 group.add_argument('-d', '--decrypt')
+
+parser.add_argument('-v', '--video', action='store_true')
 
 parser.add_argument('-o', '--output', type=str,
                     help='Output file name')
@@ -45,7 +48,56 @@ def show_image(img, scale=args.factor):
     cv2.destroyAllWindows()
 
 
-def encrypt(seed, file_name, save_name=args.output):
+executor = ThreadPoolExecutor(max_workers=8)
+
+
+def encrypt_video():
+    vidcap = cv2.VideoCapture(args.encrypt)
+    frame_rate = vidcap.get(cv2.CAP_PROP_FPS)
+    success, image = vidcap.read()
+    count = 0
+    success = True
+    if not os.path.exists('./frames/'):
+        os.makedirs('./frames/')
+    while success:
+        success, image = vidcap.read()
+        cv2.imwrite("./frames/frame%d.jpg" %
+                    count, image)     # save frame as JPEG file
+        if cv2.waitKey(10) == 27:                     # exit if Escape is hit
+            break
+        count += 1
+
+    audio = AudioSegment.from_file(args.encrypt)
+    number_files = sum([len(files) for r, d, files in os.walk("./frames/")])
+    audioArr = []
+    currAudio = 0
+    audio_s = (audio.duration_seconds * 1000) / number_files
+    while int(currAudio) <= audio.duration_seconds * 1000:
+        currAudio += audio_s
+        audioArr.append(audio[currAudio-audio_s:currAudio])
+
+    random.seed(args.seed)
+    random.shuffle(audioArr)
+    shuffledAudio = AudioSegment.empty()
+    for audioSegment in audioArr:
+        shuffledAudio += audioSegment
+
+    shuffledAudio.export("test.mp3", format='mp3')
+    seedLength = 0
+    if not os.path.exists('./encryptedFrames/'):
+        os.makedirs('./encryptedFrames/')
+
+    for count, frame in enumerate(sorted(os.listdir('./frames/')), start=1):
+        executor.submit(encrypt, args.seed+str(seedLength),
+                        './frames/'+frame, './encryptedFrames/'+frame)
+
+        seedLength += 1
+
+    executor.shutdown(wait=True)
+
+
+def encrypt(seed, file_name, save_name):
+    print(file_name)
     img_show = cv2.imread(file_name, args.color)
 
     height, width = img_show.shape[:2]
@@ -65,7 +117,7 @@ def encrypt(seed, file_name, save_name=args.output):
     print("Pixels loaded into list;")
     random.seed(seed)
     random.shuffle(myBlocks)
-    print("Shuffled with seed {}".format(args.seed))
+    print("Shuffled with seed {}".format(seed))
     blockId = -1
     pixelId = 0
     for blockX in range(int(width / args.pixel)):
@@ -77,9 +129,8 @@ def encrypt(seed, file_name, save_name=args.output):
                     blank_image[y, x] = myBlocks[blockId][pixelId]
                     pixelId += 1
     if args.output:
-        cv2.imwrite(save_name, blank_image, [
-                    cv2.IMWRITE_PNG_COMPRESSION, 0, cv2.IMWRITE_JPEG_QUALITY, 100])
-        print("Saved as {}".format(args.output))
+        cv2.imwrite(save_name, blank_image)
+        print("Saved as {}".format(save_name))
     return blank_image
 
 
@@ -131,9 +182,12 @@ def decrypt(seed, file_name, save_name=args.output):
 
 if __name__ == '__main__':
     seed = args.seed
-    if args.encrypt:
-        img = encrypt(seed, args.encrypt)
-        show_image(img)
-    if args.decrypt:
-        img = decrypt(seed, args.decrypt)
-        show_image(img)
+    if args.video:
+        encrypt_video()
+    else:
+        if args.encrypt:
+            img = encrypt(seed, args.encrypt)
+            show_image(img)
+        if args.decrypt:
+            img = decrypt(seed, args.decrypt)
+            show_image(img)
